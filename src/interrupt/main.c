6f6ded382c28964b8bf4262472c14d1d9bd62b67
William Emfinger <emfinger@isis.vanderbilt.edu>
2016-09-10 16:05:32 -0700

    added comments to interrupt code.

--- src/interrupt/main.c
+++ src/interrupt/main.c
@@ -11,7 +11,7 @@ int main() {
   WDTCTL = WDTPW + WDTHOLD; // Stop WatchDogTimer (WDT)
 
   BCSCTL1 = CALBC1_1MHZ;
-  DCOCTL = CALDCO_1MHZ;
+  DCOCTL = CALDCO_1MHZ; // set 1 MHz clock
 
   TA0CCR0 = 25000;
   TA0CCTL0 = CCIE;
@@ -20,12 +20,12 @@ int main() {
   P1REN |= BIT3;
   P1IES |= BIT3;
   P1IFG &= ~BIT3;
-  P1IE |= BIT3;
+  P1IE |= BIT3;  // enable interrupt on P1.3 (switch on the board)
 
-  P1DIR |= BIT0 | BIT6; // P1.6 is LED 2 on the board
+  P1DIR |= BIT0 | BIT6; // P1.0 is LED 1, & P1.6 is LED 2 on the board
   P1OUT &= ~( BIT0 | BIT6 );
 
-  __bis_SR_register( GIE );
+  __bis_SR_register( GIE );  // general interrupt enable (GIE)
 
   while(1) {
   }
@@ -36,18 +36,18 @@ int main() {
 void Port1Int(void) {
   switch(P1IFG & BIT3) {
   case BIT3:
-    P1IFG &= ~BIT3;
+    P1IFG &= ~BIT3;  // clear interrupt flag
     TA0CTL = TASSEL_2 | ID_2 | MC_1; // run timer
     break;
   }
 }
 
 void TimerA0Int(void) {
-  P1OUT ^= BIT6;
+  P1OUT ^= BIT6; // flash LED 2 every timer tick
   if ( counter == 5 ) {
     counter = 0;
     seconds++;
-    P1OUT ^= BIT0;
+    P1OUT ^= BIT0; // flash LED 1 every second
     if (seconds > 7) {
       TA0CTL = 0; // stop timer
       seconds = 0;
